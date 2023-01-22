32276b886c07c4048e235e6ec771d4ee1e671147
Atharva patle <atharvapatle001@gmail.com>
2023-01-22 11:16:40 +0530

    Hello

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
+import React from 'react'
+import {IncomeExpenses} from './Components/IncomeExpenses'
 import './App.css';
+import { Balance } from './Components/Balance';
+import { Header } from './Components/Header';
+import { TransactionList } from './Components/TransactionList';
+import { AddTransaction } from './Components/AddTransaction';
+
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div >
+      <Header/>
+      <div className="container">
+        <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
+        
+      </div>
     </div>
   );
 }
